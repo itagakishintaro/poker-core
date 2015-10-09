2dd7837631a26c7965f598e0acf94d3c93d520d5
itagaki <itagaki.shintaro@gmail.com>
2015-10-09 20:12:37 +0900

    石野田： 　配列の宣言と、格納用のループを記述しました！

--- src/poker-core.js
+++ src/poker-core.js
@@ -9,16 +9,16 @@ var poker = {};
  * @enum {number}
  */
 poker.handCategory = {
-  HIGH_CARD       :   0,
-  ONE_PAIR        : 100,
-  TWO_PAIR        : 200,
-  THREE_OF_A_KIND : 300,
-  STRAIGHT        : 400,
-  FLUSH           : 500,
-  FULL_HOUSE      : 600,
-  FOUR_OF_A_KIND  : 700,
-  STRAIGHT_FLUSH  : 800,
-  ROYAL_FLUSH     : 900
+  HIGH_CARD: 0,
+  ONE_PAIR: 100,
+  TWO_PAIR: 200,
+  THREE_OF_A_KIND: 300,
+  STRAIGHT: 400,
+  FLUSH: 500,
+  FULL_HOUSE: 600,
+  FOUR_OF_A_KIND: 700,
+  STRAIGHT_FLUSH: 800,
+  ROYAL_FLUSH: 900
 };
 
 
@@ -35,8 +35,24 @@ poker.core = {};
  * @param {{rank:number, suit:string}[]} cards 手札。
  * @returns poker.handCategory のどれか。
  */
-poker.core.getHandCategory = function(cards) {
+poker.core.getHandCategory = function ( cards ) {
   // TODO: ここに処理を実装します。
+
+  //配列の宣言
+  var ranks = []; //番号です！
+  var suits = []; //マークです！
+
+  for ( var i = 0; i < cards.length; i++ ) {
+    //ここに手札（cardsの配列）を格納してください！
+
+
+  }
+
+
+
+
+
+
 
 
 
